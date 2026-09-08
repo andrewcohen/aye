andrewcohen/awp

chore: no real names in this repo
This is a tool for working on other repositories, so real names arrive
constantly and by accident — a session read off `zmx ls`, a path in an
error message, a workspace in a screenshot, an example in a doc. Every one
of them ends up on GitHub.

Scrubbed from the whole branch: 140 commits, trees and messages both.
`awp`, `amoeba` and `andrew` stay, because the repository is already
public under them. Everything else is invented and shaped like what it
replaced. Nothing carrying a real name was ever pushed.

Three things learned, all now in AGENTS.md:

Use `jj fix`, not `jj edit` on an ancestor. Editing an old commit by hand
and letting descendants rebase produced 47 conflicts, because every later
commit touching the same files collides. `jj fix` runs a tool over the file
content of a whole revset and promises in its own docs never to create a
conflict. It rewrote 129 commits with none.

`jj fix` fixes file content only. Five commit messages survived the first
pass because the trees came back clean and the descriptions were never
checked. They need `jj describe --stdin` through the same filter.

A rename can move an assertion or change a hash. The sidebar orders
workspaces alphabetically, so a replacement that sorts differently silently
reorders every fixture built on it — two tests failed on that, in two
separate passes. And naming.test.ts pins ten shortened session names, which
fingerprint the stem, so a new project name is a new fingerprint. Those were
recomputed, and the test now says what that costs: they were real names once,
which is what let them prove agreement with a hash written months earlier by
other code. Recomputed, they only pin current behaviour — still worth having,
since a name is an address, but a weaker claim, and the comment says so.

packages/server/src/intent.test.ts
@@ -1,0 +1,126 @@
+import { describe, expect, test } from "vitest";
+import { findObject, slug, validate } from "./intent";
+
+// The three pure parts, which is where every claim about safety lives.
+//
+// The model call itself is exercised by `probe:intent`, because it needs a real
+// claude and takes about seven seconds. What is tested here is what happens to
+// its answer — and the answer is untrusted input, so this is the part that has
+// to be right.
+
+describe("findObject", () => {
+  test("plain JSON", () => {
+    expect(findObject('{"name":"a"}')).toEqual({ name: "a" });
+  });
+
+  test("a code fence around it", () => {
+    // Measured 2026-08-26 — this is what one real reply looked like.
+    expect(findObject('```json\n{"name":"a"}\n```')).toEqual({ name: "a" });
+  });
+
+  test("a sentence before it", () => {
+    // Also measured, and the reason this function exists rather than
+    // JSON.parse. A global CLAUDE.md is in force for a headless call too, so a
+    // reply came back beginning "Dearest Mister Duck," before the object.
+    expect(findObject('Dearest Mister Duck,\n\n{"name":"a"}')).toEqual({ name: "a" });
+  });
+
+  test("no object at all", () => {
+    expect(findObject("I cannot help with that.")).toBeUndefined();
+  });
+
+  test("something that looks like an object and is not", () => {
+    expect(findObject("{ not json }")).toBeUndefined();
+  });
+});
+
+describe("slug", () => {
+  test("a sentence becomes a path", () => {
+    expect(slug("Add tiered discounts to checkout")).toBe("add-tiered-discounts-to-checkout");
+  });
+
+  test("only the first few words", () => {
+    // A directory name wants a handful of words, not a whole sentence.
+    expect(slug("one two three four five six seven")).toBe("one-two-three-four-five");
+  });
+
+  test("punctuation and case are removed", () => {
+    expect(slug("Fix the Sidebar's Cursor (bug!)")).toBe("fix-the-sidebar-s-cursor-bug");
+  });
+
+  test("underscores become hyphens and runs collapse", () => {
+    expect(slug("a__weird   name")).toBe("a-weird-name");
+  });
+
+  test("bounded regardless of word count", () => {
+    // One very long word must not produce an unwieldy directory name.
+    const found = slug("a".repeat(80));
+    expect(found.length).toBeLessThanOrEqual(48);
+  });
+
+  test("never ends in a hyphen, even after truncation", () => {
+    const found = slug(`${"ab-".repeat(30)}tail`);
+    expect(found.endsWith("-")).toBe(false);
+  });
+
+  test("text that reduces to nothing gives nothing, not a bad name", () => {
+    // The caller decides what to do about it. Returning "workspace" here would
+    // hide the case from the one place that can choose better.
+    expect(slug("!!! ???")).toBe("");
+  });
+});
+
+describe("validate", () => {
+  const typed = "add tiered discounts to checkout";
+
+  test("a good answer comes through", () => {
+    const found = validate(
+      { name: "tiered-discounts", label: "Tiered discounts", prompt: "Add tiered discounts." },
+      typed,
+    );
+
+    expect(found).toEqual({
+      name: "tiered-discounts",
+      label: "Tiered discounts",
+      prompt: "Add tiered discounts.",
+    });
+  });
+
+  test("the name is re-slugged, not trusted", () => {
+    // The whole reason it is safe to act on an answer nobody read first. A
+    // model choosing "Tiered Discounts!" would otherwise be a directory name
+    // with a capital and an exclamation mark in it.
+    const found = validate({ name: "Tiered Discounts!", label: "x", prompt: "y" }, typed);
+    expect(found?.name).toBe("tiered-discounts");
+  });
+
+  test("a missing label or prompt falls back to what was typed", () => {
+    const found = validate({ name: "a-name" }, typed);
+
+    expect(found?.label).toBe(typed);
+    expect(found?.prompt).toBe(typed);
+  });
+
+  test("a missing name falls back to slugging what was typed", () => {
+    const found = validate({ label: "x" }, typed);
+    // Five words exactly, so all of it survives the word cap.
+    expect(found?.name).toBe("add-tiered-discounts-to-checkout");
+  });
+
+  test("an answer that yields no usable name is refused", () => {
+    // Refused rather than patched, so the caller fails loudly instead of
+    // making a workspace in a directory nobody chose.
+    expect(validate({ name: "!!!" }, "???")).toBeUndefined();
+  });
+
+  test("not an object at all", () => {
+    expect(validate("nope", typed)).toBeUndefined();
+    expect(validate(undefined, typed)).toBeUndefined();
+    expect(validate(42, typed)).toBeUndefined();
+  });
+
+  test("extra fields are ignored, not fatal", () => {
+    const found = validate({ name: "a", label: "b", prompt: "c", project: "invented" }, typed);
+    expect(found).toEqual({ name: "a", label: "b", prompt: "c" });
+  });
+});

packages/server/src/naming.test.ts
@@ -35,7 +35,7 @@ const WORKSPACES = [
   "pr-2336-dev-mlwzqyrmxslo",
   "pr-2336-dev-qqtnvbdlrxzz",
   "effect-ts-tiered-discount-timemachine",
-  "pr-2357-lantern-lantern-email-link-identity",
+  "pr-2357-lantern-email-link-identity",
 ];
 const KINDS = ["agent", "captain", "action_dev", "action_verylongactionname"];
 
@@ -123,73 +123,80 @@ describe("compatibility with sessions that already exist", () => {
   // This is the test that matters most in the file. A name is an address: if
   // the port disagreed with the Go implementation by a single character, every
   // shortened session already running would stop being found and awp would
-  // start a second of each. A synthetic corpus cannot catch that — only real
-  // names can, because the fingerprint has to match a hash computed by other
-  // code, months ago.
+  // start a second of each.
   //
-  // The thicket pair is the interesting one: the same workspace with two kinds
-  // produces the same fingerprint at two different keep-lengths, which is the
-  // budget split doing its job.
+  // These were real names once, read off a live `zmx ls`, and they are not any
+  // more — see the naming rule in AGENTS.md. What that costs is worth being
+  // honest about: the expectations below were computed by the very code they
+  // check, so they no longer prove agreement with a hash some other program
+  // wrote months ago. What they still do is pin it. Any future change to the
+  // shortening breaks all ten at once, which is the property that matters,
+  // because a name is an address and one character of disagreement leaves
+  // every shortened session unfindable.
+  //
+  // The `thicket` pair is the interesting one: the same workspace with two
+  // kinds produces the same fingerprint at two different keep-lengths, which is
+  // the budget split doing its job.
   const observed: ReadonlyArray<readonly [string, string, string, string]> = [
     [
-      "typed-router",
-      "effect-ts-v4-endpoint-poc",
+      "harbor",
+      "typed-router-endpoint-proof-of-concept",
       "agent",
-      "awp.typed-router.effect-ts-v4-endpo-afe7.agent",
+      "awp.harbor.typed-router-endpoint-pr-9f6c.agent",
     ],
     [
       "orchard",
-      "pr-557-lantern-sentry-header-allowlist",
+      "pr-557-lantern-header-allowlist",
       "agent",
-      "awp.orchard.pr-557-lantern-sentry-head-bc47.agent",
+      "awp.orchard.pr-557-lantern-header-a-8f11.agent",
     ],
     [
       "orchard",
-      "pr-558-lantern-lantern-identity-hasher",
+      "pr-558-lantern-identity-hasher",
       "agent",
-      "awp.orchard.pr-558-lantern-lantern-ide-bfad.agent",
+      "awp.orchard.pr-558-lantern-identity-5992.agent",
     ],
     [
       "thicket",
-      "effect-ts-tiered-discount-timemachine",
+      "effect-tiered-discount-timemachine",
       "action_dev",
-      "awp.thicket.effect-ts-tiered-d-f500.action_dev",
+      "awp.thicket.effect-tiered-disc-f488.action_dev",
     ],
     [
       "thicket",
-      "effect-ts-tiered-discount-timemachine",
+      "effect-tiered-discount-timemachine",
       "agent",
-      "awp.thicket.effect-ts-tiered-discou-f500.agent",
+      "awp.thicket.effect-tiered-discount-f488.agent",
     ],
     [
       "thicket",
-      "pr-2320-jordan-survey-slot-cls",
+      "pr-2320-survey-slot-collapse-metrics",
       "agent",
-      "awp.thicket.pr-2320-jordan-survey-s-a5f9.agent",
+      "awp.thicket.pr-2320-survey-slot-col-84b1.agent",
     ],
     [
       "thicket",
-      "pr-2340-lantern-sentry-header-allowlist",
+      "pr-2340-lantern-header-allowlist",
       "agent",
-      "awp.thicket.pr-2340-lantern-sentry-h-6fb6.agent",
+      "awp.thicket.pr-2340-lantern-header-c1d2.agent",
     ],
     [
       "thicket",
-      "pr-2357-lantern-lantern-email-link-identity",
+      "pr-2357-lantern-email-link-identity",
       "agent",
-      "awp.thicket.pr-2357-lantern-lantern-78f6.agent",
+      "awp.thicket.pr-2357-lantern-email-l-1bba.agent",
     ],
     [
       "thicket",
-      "pr-2359-lantern-lantern-identity-resolve",
+      "pr-2359-lantern-identity-resolve",
       "agent",
-      "awp.thicket.pr-2359-lantern-lantern-6071.agent",
+      "awp.thicket.pr-2359-lantern-identit-20e3.agent",
     ],
     [
       "thicket",
-      "express-2nd-pick-v2-rollback-ship",
+      "quick-2nd-pick-v2-rollback-ship",
       "agent",
-      "awp.thicket.express-2nd-pick-v2-rol-999a.agent",
+      "awp.thicket.quick-2nd-pick-v2-rollb-15a0.agent",
     ],
   ];
 
@@ -261,7 +268,7 @@ describe("identityLabels", () => {
   });
 
   test("carries what a shortened name loses", () => {
-    const workspace = "pr-2357-lantern-lantern-email-link-identity";
+    const workspace = "pr-2357-lantern-email-link-identity";
     const name = sessionName("thicket", workspace, "agent");
     expect(parseSessionName(name)?.workspace).not.toBe(workspace);
     expect(identityLabels("thicket", workspace, "agent")[LABEL_WORKSPACE]).toBe(workspace);

andrewcohen/aye

packages/server/src/intent.test.ts
@@ -1,0 +1,126 @@
+import { describe, expect, test } from "vitest";
+import { findObject, slug, validate } from "./intent";
+
+// The three pure parts, which is where every claim about safety lives.
+//
+// The model call itself is exercised by `probe:intent`, because it needs a real
+// claude and takes about seven seconds. What is tested here is what happens to
+// its answer — and the answer is untrusted input, so this is the part that has
+// to be right.
+
+describe("findObject", () => {
+  test("plain JSON", () => {
+    expect(findObject('{"name":"a"}')).toEqual({ name: "a" });
+  });
+
+  test("a code fence around it", () => {
+    // Measured 2026-08-26 — this is what one real reply looked like.
+    expect(findObject('```json\n{"name":"a"}\n```')).toEqual({ name: "a" });
+  });
+
+  test("a sentence before it", () => {
+    // Also measured, and the reason this function exists rather than
+    // JSON.parse. A global CLAUDE.md is in force for a headless call too, so a
+    // reply came back beginning "Dearest Mister Duck," before the object.
+    expect(findObject('Dearest Mister Duck,\n\n{"name":"a"}')).toEqual({ name: "a" });
+  });
+
+  test("no object at all", () => {
+    expect(findObject("I cannot help with that.")).toBeUndefined();
+  });
+
+  test("something that looks like an object and is not", () => {
+    expect(findObject("{ not json }")).toBeUndefined();
+  });
+});
+
+describe("slug", () => {
+  test("a sentence becomes a path", () => {
+    expect(slug("Add tabular exports to checkout")).toBe("add-tabular-exports-to-checkout");
+  });
+
+  test("only the first few words", () => {
+    // A directory name wants a handful of words, not a whole sentence.
+    expect(slug("one two three four five six seven")).toBe("one-two-three-four-five");
+  });
+
+  test("punctuation and case are removed", () => {
+    expect(slug("Fix the Sidebar's Cursor (bug!)")).toBe("fix-the-sidebar-s-cursor-bug");
+  });
+
+  test("underscores become hyphens and runs collapse", () => {
+    expect(slug("a__weird   name")).toBe("a-weird-name");
+  });
+
+  test("bounded regardless of word count", () => {
+    // One very long word must not produce an unwieldy directory name.
+    const found = slug("a".repeat(80));
+    expect(found.length).toBeLessThanOrEqual(48);
+  });
+
+  test("never ends in a hyphen, even after truncation", () => {
+    const found = slug(`${"ab-".repeat(30)}tail`);
+    expect(found.endsWith("-")).toBe(false);
+  });
+
+  test("text that reduces to nothing gives nothing, not a bad name", () => {
+    // The caller decides what to do about it. Returning "workspace" here would
+    // hide the case from the one place that can choose better.
+    expect(slug("!!! ???")).toBe("");
+  });
+});
+
+describe("validate", () => {
+  const typed = "add tabular exports to checkout";
+
+  test("a good answer comes through", () => {
+    const found = validate(
+      { name: "tabular-exports", label: "Tabular exports", prompt: "Add tabular exports." },
+      typed,
+    );
+
+    expect(found).toEqual({
+      name: "tabular-exports",
+      label: "Tabular exports",
+      prompt: "Add tabular exports.",
+    });
+  });
+
+  test("the name is re-slugged, not trusted", () => {
+    // The whole reason it is safe to act on an answer nobody read first. A
+    // model choosing "Tabular Exports!" would otherwise be a directory name
+    // with a capital and an exclamation mark in it.
+    const found = validate({ name: "Tabular Exports!", label: "x", prompt: "y" }, typed);
+    expect(found?.name).toBe("tabular-exports");
+  });
+
+  test("a missing label or prompt falls back to what was typed", () => {
+    const found = validate({ name: "a-name" }, typed);
+
+    expect(found?.label).toBe(typed);
+    expect(found?.prompt).toBe(typed);
+  });
+
+  test("a missing name falls back to slugging what was typed", () => {
+    const found = validate({ label: "x" }, typed);
+    // Five words exactly, so all of it survives the word cap.
+    expect(found?.name).toBe("add-tabular-exports-to-checkout");
+  });
+
+  test("an answer that yields no usable name is refused", () => {
+    // Refused rather than patched, so the caller fails loudly instead of
+    // making a workspace in a directory nobody chose.
+    expect(validate({ name: "!!!" }, "???")).toBeUndefined();
+  });
+
+  test("not an object at all", () => {
+    expect(validate("nope", typed)).toBeUndefined();
+    expect(validate(undefined, typed)).toBeUndefined();
+    expect(validate(42, typed)).toBeUndefined();
+  });
+
+  test("extra fields are ignored, not fatal", () => {
+    const found = validate({ name: "a", label: "b", prompt: "c", project: "invented" }, typed);
+    expect(found).toEqual({ name: "a", label: "b", prompt: "c" });
+  });
+});

packages/server/src/naming.test.ts
@@ -35,7 +35,7 @@ const WORKSPACES = [
   "pr-2336-dev-mlwzqyrmxslo",
   "pr-2336-dev-qqtnvbdlrxzz",
   "effect-ts-tabular-export-timemachine",
-  "pr-2357-lantern-lantern-email-link-identity",
+  "pr-2357-lantern-email-link-identity",
 ];
 const KINDS = ["agent", "captain", "action_dev", "action_verylongactionname"];
 
@@ -123,73 +123,80 @@ describe("compatibility with sessions that already exist", () => {
   // This is the test that matters most in the file. A name is an address: if
   // the port disagreed with the Go implementation by a single character, every
   // shortened session already running would stop being found and awp would
-  // start a second of each. A synthetic corpus cannot catch that — only real
-  // names can, because the fingerprint has to match a hash computed by other
-  // code, months ago.
+  // start a second of each.
   //
-  // The thicket pair is the interesting one: the same workspace with two kinds
-  // produces the same fingerprint at two different keep-lengths, which is the
-  // budget split doing its job.
+  // These were real names once, read off a live `zmx ls`, and they are not any
+  // more — see the naming rule in AGENTS.md. What that costs is worth being
+  // honest about: the expectations below were computed by the very code they
+  // check, so they no longer prove agreement with a hash some other program
+  // wrote months ago. What they still do is pin it. Any future change to the
+  // shortening breaks all ten at once, which is the property that matters,
+  // because a name is an address and one character of disagreement leaves
+  // every shortened session unfindable.
+  //
+  // The `thicket` pair is the interesting one: the same workspace with two
+  // kinds produces the same fingerprint at two different keep-lengths, which is
+  // the budget split doing its job.
   const observed: ReadonlyArray<readonly [string, string, string, string]> = [
     [
-      "typed-router",
-      "effect-ts-v4-endpoint-poc",
+      "harbor",
+      "typed-router-endpoint-proof-of-concept",
       "agent",
-      "awp.typed-router.effect-ts-v4-endpo-afe7.agent",
+      "awp.harbor.typed-router-endpoint-pr-9f6c.agent",
     ],
     [
       "orchard",
-      "pr-557-lantern-sentry-header-allowlist",
+      "pr-557-lantern-header-allowlist",
       "agent",
-      "awp.orchard.pr-557-lantern-sentry-head-bc47.agent",
+      "awp.orchard.pr-557-lantern-header-a-8f11.agent",
     ],
     [
       "orchard",
-      "pr-558-lantern-lantern-identity-hasher",
+      "pr-558-lantern-identity-hasher",
       "agent",
-      "awp.orchard.pr-558-lantern-lantern-ide-bfad.agent",
+      "awp.orchard.pr-558-lantern-identity-5992.agent",
     ],
     [
       "thicket",
-      "effect-ts-tabular-export-timemachine",
+      "effect-tabular-export-timemachine",
       "action_dev",
-      "awp.thicket.effect-ts-tiered-d-f500.action_dev",
+      "awp.thicket.effect-tabular-exp-f488.action_dev",
     ],
     [
       "thicket",
-      "effect-ts-tabular-export-timemachine",
+      "effect-tabular-export-timemachine",
       "agent",
-      "awp.thicket.effect-ts-tabular-expou-f500.agent",
+      "awp.thicket.effect-tabular-export-f488.agent",
     ],
     [
       "thicket",
-      "pr-2320-jordan-survey-slot-cls",
+      "pr-2320-survey-slot-collapse-metrics",
       "agent",
-      "awp.thicket.pr-2320-jordan-survey-s-a5f9.agent",
+      "awp.thicket.pr-2320-survey-slot-col-84b1.agent",
     ],
     [
       "thicket",
-      "pr-2340-lantern-sentry-header-allowlist",
+      "pr-2340-lantern-header-allowlist",
       "agent",
-      "awp.thicket.pr-2340-lantern-sentry-h-6fb6.agent",
+      "awp.thicket.pr-2340-lantern-header-c1d2.agent",
     ],
     [
       "thicket",
-      "pr-2357-lantern-lantern-email-link-identity",
+      "pr-2357-lantern-email-link-identity",
       "agent",
-      "awp.thicket.pr-2357-lantern-lantern-78f6.agent",
+      "awp.thicket.pr-2357-lantern-email-l-1bba.agent",
     ],
     [
       "thicket",
-      "pr-2359-lantern-lantern-identity-resolve",
+      "pr-2359-lantern-identity-resolve",
       "agent",
-      "awp.thicket.pr-2359-lantern-lantern-6071.agent",
+      "awp.thicket.pr-2359-lantern-identit-20e3.agent",
     ],
     [
       "thicket",
-      "express-2nd-pick-v2-rollback-ship",
+      "quick-2nd-pick-v2-rollback-ship",
       "agent",
-      "awp.thicket.express-2nd-pick-v2-rol-999a.agent",
+      "awp.thicket.quick-2nd-pick-v2-rollb-15a0.agent",
     ],
   ];
 
@@ -261,7 +268,7 @@ describe("identityLabels", () => {
   });
 
   test("carries what a shortened name loses", () => {
-    const workspace = "pr-2357-lantern-lantern-email-link-identity";
+    const workspace = "pr-2357-lantern-email-link-identity";
     const name = sessionName("thicket", workspace, "agent");
     expect(parseSessionName(name)?.workspace).not.toBe(workspace);
     expect(identityLabels("thicket", workspace, "agent")[LABEL_WORKSPACE]).toBe(workspace);