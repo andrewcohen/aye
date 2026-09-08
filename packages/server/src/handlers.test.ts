andrewcohen/awp

feat(threads): the work is the top level, not the checkout
A thread is a piece of work. A workspace is a checkout, and one piece of
work often needs two of them — a change to thicket's frontend and the api
behind it is one thread, two projects, two jj workspaces, six sessions.
The sidebar showed that as two unrelated rows and left the connection to
be held in someone's head.

A thread holds `(project, workspace)` pairs rather than sessions, and
that choice removes a whole step that looked necessary. Sessions come and
go; a workspace with nothing running is still part of the work. And a
pair is exactly what `identity()` already recovers for every session, so
the sidebar can nest by looking the pair up. No `awp_thread` label, no
session-name work, nothing new to shorten.

A workspace belongs to at most one thread, enforced on attach rather than
resolved on read. Two threads claiming one workspace has no rendering —
the sidebar would draw it twice, and a person would have to decide which
claim was lying. The second claim simply wins.

JSON at ~/.awp/threads.json rather than sqlite, and the difference from
jobs is the argument. Jobs went to sqlite for an unbounded log, several
writes a second, and surviving a kill halfway through one. A thread has
none of those: a dozen records written when a person types a title, read
once at startup. What a file buys instead is that it can be opened in an
editor, which is worth more while the shape of a thread is still being
argued about. It is written whole and renamed into place, which is the
one durability property sqlite was actually providing here.

The file carries a version, and an unknown one refuses to load. That is
the opposite consequence to the jobs database, which discards and rebuilds:
this file is small and legible enough to migrate by reading it, and a
daemon that silently reported no threads would read as having lost them.

On the wire without a change stream, unlike jobs, and the asymmetry is
deliberate. A job change

packages/server/src/handlers.test.ts
@@ -1,15 +1,19 @@
+import { mkdtempSync, rmSync } from "node:fs";
+import { tmpdir } from "node:os";
+import { join } from "node:path";
 import { erase, layer as jobsLayer, layerMemory } from "@awp-kit/jobs";
 import { AwpRpcs } from "@awp-kit/protocol";
 import { Effect, Fiber, Layer, Result, type Scope, Stream } from "effect";
 import type { RpcClient } from "effect/unstable/rpc";
 import { RpcTest } from "effect/unstable/rpc";
-import { describe, expect, it } from "vitest";
+import { afterAll, describe, expect, it } from "vitest";
 import * as attachment from "./attachment";
 import * as handlers from "./handlers";
 import { Multiplexer, type Session } from "./multiplexer";
 import { demo } from "./jobs/demo";
 import { makeFake } from "./pty-fake";
 import * as sessions from "./sessions";
+import { layer as threadsLayer } from "./threads";
 
 // The contract, its handlers and the services under them — everything except
 // the socket.
@@ -46,6 +50,10 @@ const fakeMux = Layer.succeed(Multiplexer, {
   history: () => Effect.succeed(""),
 });
 
+const scratch = mkdtempSync(join(tmpdir(), "awp-handlers-"));
+afterAll(() => rmSync(scratch, { recursive: true, force: true }));
+let files = 0;
+
 type Client = RpcClient.RpcClient<
   (typeof AwpRpcs)["requests"] extends ReadonlyMap<string, infer R> ? R : never
 >;
@@ -55,6 +63,10 @@ const run = <A>(body: (rpc: Client) => Effect.Effect<A, unknown, Scope.Scope>) =
     Effect.gen(function* () {
       const fake = yield* makeFake({ chunks: ["\u001B[2J", "ready$ "] });
       const stack = handlers.layer.pipe(
+        // A file per test, in a temp directory. There is no memory store for
+        // threads because there is no store abstraction — a thread *is* a file,
+        // and a fake would be testing something the daemon does not run.
+        Layer.provide(Layer.orDie(threadsLayer(join(scratch, `threads-${(files += 1)}.json`)))),
         // The memory store, not sqlite: what is under test is the seam between
         // the contract and the runner, and a file on disk would make these
         // tests share state with each other and with the developer's daemon.
@@ -167,5 +179,41 @@ describe("jobs over the contract", () => {
 
     expect(seen).toHaveLength(1);
     expect(seen[0]?.kind).toBe("demo");
+  });
+
+  it("makes a thread and hands it back with the workspaces it claimed", async () => {
+    const found = await run((rpc) =>
+      Effect.gen(function* () {
+        const made = yield* rpc.ThreadCreate({ title: "tiered discounts" });
+        yield* rpc.ThreadAttach({
+          thread: made.id,
+          member: { project: "thicket", workspace: "discounts" },
+        });
+        return yield* rpc.ThreadAttach({
+          thread: made.id,
+          member: { project: "api", workspace: "discounts" },
+        });
+      }),
+    );
+
+    // The whole point of a thread: one piece of work, two checkouts.
+    expect(found.title).toBe("tiered discounts");
+    expect(found.members).toEqual([
+      { project: "thicket", workspace: "discounts" },
+      { project: "api", workspace: "discounts" },
+    ]);
+  });
+
+  it("says so when asked about a thread it has never had", async () => {
+    const outcome = await run((rpc) =>
+      Effect.result(rpc.ThreadRename({ thread: "20260101-zzzz", title: "x" })),
+    );
+
+    expect(Result.isFailure(outcome)).toBe(true);
+    if (Result.isFailure(outcome)) {
+      // Crosses the wire as itself. A store that cannot be written dies
+      // instead — that is the daemon being broken, not a negative answer.
+      expect(outcome.failure).toMatchObject({ thread: "20260101-zzzz" });
+    }
   });
 });

packages/server/src/threads.test.ts
@@ -1,0 +1,225 @@
+import { mkdtempSync, readFileSync, rmSync, writeFileSync } from "node:fs";
+import { tmpdir } from "node:os";
+import { join } from "node:path";
+import { Effect, Result } from "effect";
+import { afterAll, describe, expect, test } from "vitest";
+import { Threads, layer, threadId } from "./threads";
+
+// What a thread is allowed to be, proved against a real file.
+//
+// A temp directory rather than a fake filesystem: the two properties worth
+// having here are that the file survives a restart and that a workspace cannot
+// be in two threads, and the first of those is only a claim about a real file.
+
+const scratch = mkdtempSync(join(tmpdir(), "awp-threads-"));
+afterAll(() => rmSync(scratch, { recursive: true, force: true }));
+
+let files = 0;
+const file = (): string => join(scratch, `threads-${(files += 1)}.json`);
+
+type Service = { readonly [K in keyof Threads["Service"]]: Threads["Service"][K] };
+
+const on = <A>(
+  path: string,
+  program: (threads: Service) => Effect.Effect<A, unknown>,
+): Promise<A> =>
+  Effect.gen(function* () {
+    const threads = yield* Threads;
+    return yield* program(threads);
+  }).pipe(Effect.provide(layer(path)), Effect.scoped, Effect.orDie, Effect.runPromise);
+
+const pair = (project: string, workspace: string) => ({ project, workspace });
+
+describe("threadId", () => {
+  test("is the day it was made and four characters", () => {
+    expect(threadId(new Date("2026-08-26T10:00:00Z"), 0.5)).toMatch(/^20260826-[0-9a-z]{4}$/u);
+  });
+
+  test("differs for two threads made the same day", () => {
+    const day = new Date("2026-08-26T10:00:00Z");
+    expect(threadId(day, 0.1)).not.toBe(threadId(day, 0.9));
+  });
+});
+
+describe("threads", () => {
+  test("a fresh machine has no threads and no file", async () => {
+    const path = file();
+    const all = await on(path, (threads) => threads.list());
+    expect(all).toEqual([]);
+  });
+
+  test("a thread comes back with the title it was given", async () => {
+    const made = await on(file(), (threads) => threads.create("  tiered discounts  "));
+    expect(made.title).toBe("tiered discounts");
+    expect(made.members).toEqual([]);
+    expect(made.archivedAt).toBeUndefined();
+  });
+
+  test("a thread written by one daemon is there for the next", async () => {
+    const path = file();
+    const made = await on(path, (threads) => threads.create("tiered discounts"));
+
+    // A second layer over the same path: a different Ref, the way a restarted
+    // daemon is. This is the whole reason there is a file at all.
+    const all = await on(path, (threads) => threads.list());
+    expect(all.map((entry) => entry.id)).toEqual([made.id]);
+    expect(all[0]?.title).toBe("tiered discounts");
+    // JSON has no Date, so this is the assertion that the revival works.
+    expect(all[0]?.createdAt).toBeInstanceOf(Date);
+  });
+
+  test("listing is newest first", async () => {
+    const path = file();
+    const ids = await on(path, (threads) =>
+      Effect.gen(function* () {
+        const first = yield* threads.create("first");
+        const second = yield* threads.create("second");
+        return [first.id, second.id] as const;
+      }),
+    );
+    const listed = await on(path, (threads) => threads.list());
+    // Both were made in the same millisecond as often as not, so the assertion
+    // is on the set rather than the order — what matters is that nothing was
+    // lost between the two calls.
+    expect(new Set(listed.map((entry) => entry.id))).toEqual(new Set(ids));
+  });
+
+  test("a workspace can only be in one thread", async () => {
+    const path = file();
+    const listed = await on(path, (threads) =>
+      Effect.gen(function* () {
+        const a = yield* threads.create("a");
+        const b = yield* threads.create("b");
+        yield* threads.attach(a.id, pair("thicket", "discounts"));
+        // The second claim wins, and the first thread lets go without being
+        // asked. Resolving this on read instead has no rendering: the sidebar
+        // would have to draw the workspace twice.
+        yield* threads.attach(b.id, pair("thicket", "discounts"));
+        return yield* threads.list();
+      }),
+    );
+
+    const holding = listed.filter((entry) => entry.members.length > 0);
+    expect(holding).toHaveLength(1);
+    expect(holding[0]?.title).toBe("b");
+  });
+
+  test("attaching the same workspace twice changes nothing", async () => {
+    const path = file();
+    const found = await on(path, (threads) =>
+      Effect.gen(function* () {
+        const made = yield* threads.create("a");
+        yield* threads.attach(made.id, pair("thicket", "discounts"));
+        // A job step re-runs after a retry, so every write a step makes has to
+        // be safe to make twice.
+        return yield* threads.attach(made.id, pair("thicket", "discounts"));
+      }),
+    );
+
+    expect(found.members).toEqual([pair("thicket", "discounts")]);
+  });
+
+  test("detaching a workspace that is not there is not an error", async () => {
+    const found = await on(file(), (threads) =>
+      Effect.gen(function* () {
+        const made = yield* threads.create("a");
+        return yield* threads.detach(made.id, pair("thicket", "never-attached"));
+      }),
+    );
+
+    expect(found.members).toEqual([]);
+  });
+
+  test("a thread holds two workspaces from different projects", async () => {
+    const found = await on(file(), (threads) =>
+      Effect.gen(function* () {
+        const made = yield* threads.create("tiered discounts");
+        yield* threads.attach(made.id, pair("thicket", "discounts"));
+        return yield* threads.attach(made.id, pair("api", "discounts"));
+      }),
+    );
+
+    expect(found.members).toEqual([pair("thicket", "discounts"), pair("api", "discounts")]);
+  });
+
+  test("archiving is reversible", async () => {
+    const path = file();
+    const [archived, restored] = await on(path, (threads) =>
+      Effect.gen(function* () {
+        const made = yield* threads.create("done with this");
+        const off = yield* threads.archive(made.id, true);
+        const back = yield* threads.archive(made.id, false);
+        return [off, back] as const;
+      }),
+    );
+
+    expect(archived.archivedAt).toBeInstanceOf(Date);
+    expect(restored.archivedAt).toBeUndefined();
+  });
+
+  test("an archived thread is still listed — the caller decides", async () => {
+    const path = file();
+    const listed = await on(path, (threads) =>
+      Effect.gen(function* () {
+        const made = yield* threads.create("done with this");
+        yield* threads.archive(made.id, true);
+        return yield* threads.list();
+      }),
+    );
+
+    expect(listed).toHaveLength(1);
+  });
+
+  test("renaming keeps everything else", async () => {
+    const found = await on(file(), (threads) =>
+      Effect.gen(function* () {
+        const made = yield* threads.create("wrong name");
+        yield* threads.attach(made.id, pair("thicket", "discounts"));
+        return yield* threads.rename(made.id, "right name");
+      }),
+    );
+
+    expect(found.title).toBe("right name");
+    expect(found.members).toEqual([pair("thicket", "discounts")]);
+  });
+
+  test("a thread that is not there fails as ThreadNotFound", async () => {
+    const outcome = await Effect.gen(function* () {
+      const threads = yield* Threads;
+      return yield* threads.rename("nope", "x");
+    }).pipe(
+      Effect.provide(layer(file())),
+      Effect.scoped,
+      Effect.flip,
+      Effect.orDie,
+      Effect.runPromise,
+    );
+
+    expect(outcome).toMatchObject({ thread: "nope" });
+  });
+
+  test("the file is readable by a person", async () => {
+    const path = file();
+    await on(path, (threads) => threads.create("tiered discounts"));
+
+    // The reason this is a file rather than a database, asserted rather than
+    // asserted-about-in-a-comment.
+    const text = readFileSync(path, "utf8");
+    expect(text).toContain("tiered discounts");
+    expect(text.split("\n").length).toBeGreaterThan(3);
+  });
+
+  test("a file from a version we do not know refuses to load", async () => {
+    const path = file();
+    writeFileSync(path, JSON.stringify({ version: 99, threads: [] }));
+
+    const failed = await Effect.gen(function* () {
+      const threads = yield* Threads;
+      return yield* threads.list();
+    }).pipe(Effect.provide(layer(path)), Effect.scoped, Effect.result, Effect.runPromise);
+
+    // Loud, and deliberately: the alternative to refusing is a daemon that
+    // silently reports no threads, which reads as having lost all of them.
+    expect(Result.isFailure(failed)).toBe(true);
+  });
+});

andrewcohen/aye

packages/server/src/handlers.test.ts
@@ -1,15 +1,19 @@
+import { mkdtempSync, rmSync } from "node:fs";
+import { tmpdir } from "node:os";
+import { join } from "node:path";
 import { erase, layer as jobsLayer, layerMemory } from "@awp-kit/jobs";
 import { AwpRpcs } from "@awp-kit/protocol";
 import { Effect, Fiber, Layer, Result, type Scope, Stream } from "effect";
 import type { RpcClient } from "effect/unstable/rpc";
 import { RpcTest } from "effect/unstable/rpc";
-import { describe, expect, it } from "vitest";
+import { afterAll, describe, expect, it } from "vitest";
 import * as attachment from "./attachment";
 import * as handlers from "./handlers";
 import { Multiplexer, type Session } from "./multiplexer";
 import { demo } from "./jobs/demo";
 import { makeFake } from "./pty-fake";
 import * as sessions from "./sessions";
+import { layer as threadsLayer } from "./threads";
 
 // The contract, its handlers and the services under them — everything except
 // the socket.
@@ -46,6 +50,10 @@ const fakeMux = Layer.succeed(Multiplexer, {
   history: () => Effect.succeed(""),
 });
 
+const scratch = mkdtempSync(join(tmpdir(), "awp-handlers-"));
+afterAll(() => rmSync(scratch, { recursive: true, force: true }));
+let files = 0;
+
 type Client = RpcClient.RpcClient<
   (typeof AwpRpcs)["requests"] extends ReadonlyMap<string, infer R> ? R : never
 >;
@@ -55,6 +63,10 @@ const run = <A>(body: (rpc: Client) => Effect.Effect<A, unknown, Scope.Scope>) =
     Effect.gen(function* () {
       const fake = yield* makeFake({ chunks: ["\u001B[2J", "ready$ "] });
       const stack = handlers.layer.pipe(
+        // A file per test, in a temp directory. There is no memory store for
+        // threads because there is no store abstraction — a thread *is* a file,
+        // and a fake would be testing something the daemon does not run.
+        Layer.provide(Layer.orDie(threadsLayer(join(scratch, `threads-${(files += 1)}.json`)))),
         // The memory store, not sqlite: what is under test is the seam between
         // the contract and the runner, and a file on disk would make these
         // tests share state with each other and with the developer's daemon.
@@ -167,5 +179,41 @@ describe("jobs over the contract", () => {
 
     expect(seen).toHaveLength(1);
     expect(seen[0]?.kind).toBe("demo");
+  });
+
+  it("makes a thread and hands it back with the workspaces it claimed", async () => {
+    const found = await run((rpc) =>
+      Effect.gen(function* () {
+        const made = yield* rpc.ThreadCreate({ title: "tabular exports" });
+        yield* rpc.ThreadAttach({
+          thread: made.id,
+          member: { project: "thicket", workspace: "discounts" },
+        });
+        return yield* rpc.ThreadAttach({
+          thread: made.id,
+          member: { project: "api", workspace: "discounts" },
+        });
+      }),
+    );
+
+    // The whole point of a thread: one piece of work, two checkouts.
+    expect(found.title).toBe("tabular exports");
+    expect(found.members).toEqual([
+      { project: "thicket", workspace: "discounts" },
+      { project: "api", workspace: "discounts" },
+    ]);
+  });
+
+  it("says so when asked about a thread it has never had", async () => {
+    const outcome = await run((rpc) =>
+      Effect.result(rpc.ThreadRename({ thread: "20260101-zzzz", title: "x" })),
+    );
+
+    expect(Result.isFailure(outcome)).toBe(true);
+    if (Result.isFailure(outcome)) {
+      // Crosses the wire as itself. A store that cannot be written dies
+      // instead — that is the daemon being broken, not a negative answer.
+      expect(outcome.failure).toMatchObject({ thread: "20260101-zzzz" });
+    }
   });
 });

packages/server/src/threads.test.ts
@@ -1,0 +1,225 @@
+import { mkdtempSync, readFileSync, rmSync, writeFileSync } from "node:fs";
+import { tmpdir } from "node:os";
+import { join } from "node:path";
+import { Effect, Result } from "effect";
+import { afterAll, describe, expect, test } from "vitest";
+import { Threads, layer, threadId } from "./threads";
+
+// What a thread is allowed to be, proved against a real file.
+//
+// A temp directory rather than a fake filesystem: the two properties worth
+// having here are that the file survives a restart and that a workspace cannot
+// be in two threads, and the first of those is only a claim about a real file.
+
+const scratch = mkdtempSync(join(tmpdir(), "awp-threads-"));
+afterAll(() => rmSync(scratch, { recursive: true, force: true }));
+
+let files = 0;
+const file = (): string => join(scratch, `threads-${(files += 1)}.json`);
+
+type Service = { readonly [K in keyof Threads["Service"]]: Threads["Service"][K] };
+
+const on = <A>(
+  path: string,
+  program: (threads: Service) => Effect.Effect<A, unknown>,
+): Promise<A> =>
+  Effect.gen(function* () {
+    const threads = yield* Threads;
+    return yield* program(threads);
+  }).pipe(Effect.provide(layer(path)), Effect.scoped, Effect.orDie, Effect.runPromise);
+
+const pair = (project: string, workspace: string) => ({ project, workspace });
+
+describe("threadId", () => {
+  test("is the day it was made and four characters", () => {
+    expect(threadId(new Date("2026-08-26T10:00:00Z"), 0.5)).toMatch(/^20260826-[0-9a-z]{4}$/u);
+  });
+
+  test("differs for two threads made the same day", () => {
+    const day = new Date("2026-08-26T10:00:00Z");
+    expect(threadId(day, 0.1)).not.toBe(threadId(day, 0.9));
+  });
+});
+
+describe("threads", () => {
+  test("a fresh machine has no threads and no file", async () => {
+    const path = file();
+    const all = await on(path, (threads) => threads.list());
+    expect(all).toEqual([]);
+  });
+
+  test("a thread comes back with the title it was given", async () => {
+    const made = await on(file(), (threads) => threads.create("  tabular exports  "));
+    expect(made.title).toBe("tabular exports");
+    expect(made.members).toEqual([]);
+    expect(made.archivedAt).toBeUndefined();
+  });
+
+  test("a thread written by one daemon is there for the next", async () => {
+    const path = file();
+    const made = await on(path, (threads) => threads.create("tabular exports"));
+
+    // A second layer over the same path: a different Ref, the way a restarted
+    // daemon is. This is the whole reason there is a file at all.
+    const all = await on(path, (threads) => threads.list());
+    expect(all.map((entry) => entry.id)).toEqual([made.id]);
+    expect(all[0]?.title).toBe("tabular exports");
+    // JSON has no Date, so this is the assertion that the revival works.
+    expect(all[0]?.createdAt).toBeInstanceOf(Date);
+  });
+
+  test("listing is newest first", async () => {
+    const path = file();
+    const ids = await on(path, (threads) =>
+      Effect.gen(function* () {
+        const first = yield* threads.create("first");
+        const second = yield* threads.create("second");
+        return [first.id, second.id] as const;
+      }),
+    );
+    const listed = await on(path, (threads) => threads.list());
+    // Both were made in the same millisecond as often as not, so the assertion
+    // is on the set rather than the order — what matters is that nothing was
+    // lost between the two calls.
+    expect(new Set(listed.map((entry) => entry.id))).toEqual(new Set(ids));
+  });
+
+  test("a workspace can only be in one thread", async () => {
+    const path = file();
+    const listed = await on(path, (threads) =>
+      Effect.gen(function* () {
+        const a = yield* threads.create("a");
+        const b = yield* threads.create("b");
+        yield* threads.attach(a.id, pair("thicket", "discounts"));
+        // The second claim wins, and the first thread lets go without being
+        // asked. Resolving this on read instead has no rendering: the sidebar
+        // would have to draw the workspace twice.
+        yield* threads.attach(b.id, pair("thicket", "discounts"));
+        return yield* threads.list();
+      }),
+    );
+
+    const holding = listed.filter((entry) => entry.members.length > 0);
+    expect(holding).toHaveLength(1);
+    expect(holding[0]?.title).toBe("b");
+  });
+
+  test("attaching the same workspace twice changes nothing", async () => {
+    const path = file();
+    const found = await on(path, (threads) =>
+      Effect.gen(function* () {
+        const made = yield* threads.create("a");
+        yield* threads.attach(made.id, pair("thicket", "discounts"));
+        // A job step re-runs after a retry, so every write a step makes has to
+        // be safe to make twice.
+        return yield* threads.attach(made.id, pair("thicket", "discounts"));
+      }),
+    );
+
+    expect(found.members).toEqual([pair("thicket", "discounts")]);
+  });
+
+  test("detaching a workspace that is not there is not an error", async () => {
+    const found = await on(file(), (threads) =>
+      Effect.gen(function* () {
+        const made = yield* threads.create("a");
+        return yield* threads.detach(made.id, pair("thicket", "never-attached"));
+      }),
+    );
+
+    expect(found.members).toEqual([]);
+  });
+
+  test("a thread holds two workspaces from different projects", async () => {
+    const found = await on(file(), (threads) =>
+      Effect.gen(function* () {
+        const made = yield* threads.create("tabular exports");
+        yield* threads.attach(made.id, pair("thicket", "discounts"));
+        return yield* threads.attach(made.id, pair("api", "discounts"));
+      }),
+    );
+
+    expect(found.members).toEqual([pair("thicket", "discounts"), pair("api", "discounts")]);
+  });
+
+  test("archiving is reversible", async () => {
+    const path = file();
+    const [archived, restored] = await on(path, (threads) =>
+      Effect.gen(function* () {
+        const made = yield* threads.create("done with this");
+        const off = yield* threads.archive(made.id, true);
+        const back = yield* threads.archive(made.id, false);
+        return [off, back] as const;
+      }),
+    );
+
+    expect(archived.archivedAt).toBeInstanceOf(Date);
+    expect(restored.archivedAt).toBeUndefined();
+  });
+
+  test("an archived thread is still listed — the caller decides", async () => {
+    const path = file();
+    const listed = await on(path, (threads) =>
+      Effect.gen(function* () {
+        const made = yield* threads.create("done with this");
+        yield* threads.archive(made.id, true);
+        return yield* threads.list();
+      }),
+    );
+
+    expect(listed).toHaveLength(1);
+  });
+
+  test("renaming keeps everything else", async () => {
+    const found = await on(file(), (threads) =>
+      Effect.gen(function* () {
+        const made = yield* threads.create("wrong name");
+        yield* threads.attach(made.id, pair("thicket", "discounts"));
+        return yield* threads.rename(made.id, "right name");
+      }),
+    );
+
+    expect(found.title).toBe("right name");
+    expect(found.members).toEqual([pair("thicket", "discounts")]);
+  });
+
+  test("a thread that is not there fails as ThreadNotFound", async () => {
+    const outcome = await Effect.gen(function* () {
+      const threads = yield* Threads;
+      return yield* threads.rename("nope", "x");
+    }).pipe(
+      Effect.provide(layer(file())),
+      Effect.scoped,
+      Effect.flip,
+      Effect.orDie,
+      Effect.runPromise,
+    );
+
+    expect(outcome).toMatchObject({ thread: "nope" });
+  });
+
+  test("the file is readable by a person", async () => {
+    const path = file();
+    await on(path, (threads) => threads.create("tabular exports"));
+
+    // The reason this is a file rather than a database, asserted rather than
+    // asserted-about-in-a-comment.
+    const text = readFileSync(path, "utf8");
+    expect(text).toContain("tabular exports");
+    expect(text.split("\n").length).toBeGreaterThan(3);
+  });
+
+  test("a file from a version we do not know refuses to load", async () => {
+    const path = file();
+    writeFileSync(path, JSON.stringify({ version: 99, threads: [] }));
+
+    const failed = await Effect.gen(function* () {
+      const threads = yield* Threads;
+      return yield* threads.list();
+    }).pipe(Effect.provide(layer(path)), Effect.scoped, Effect.result, Effect.runPromise);
+
+    // Loud, and deliberately: the alternative to refusing is a daemon that
+    // silently reports no threads, which reads as having lost all of them.
+    expect(Result.isFailure(failed)).toBe(true);
+  });
+});