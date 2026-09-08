andrewcohen/awp

feat(server): the Multiplexer, and zmx behind it
The first Effect service. A tag naming what can be asked about sessions
— list, lookup, kill, labels, history — with zmx.ts the only file that
knows the answer comes from zmx.

Attach is deliberately not on it. Everything here answers a question and
changes nothing, so asking costs nothing: no client opens, no session
resizes, nothing redraws. Attach is an act with a consequence, and that
same line is the line between what can be tested from inside a session
and what cannot. So the read side gets ten tests against a live zmx,
today, in the session doing the work.

Reading zmx ls is where the bugs would be, so the parser is pure and its
fixtures are real output rather than invented: a start_dir containing a
space, a cmd containing quotes, a cmd zmx truncated itself, rows with and
without awp labels. Fields split on the first = only, since cmd=FOO=bar
prog is an ordinary command line.

Stripping ZMX_SESSION from every zmx child turns out to do more than
avoid the attach hijack. zmx ls marks the caller own session with an
arrow, and that arrow breaks the first field of exactly one line — so the
session that fails to parse is always the caller own, and the workspace a
developer looks at first reads as empty. Without the marker in the
environment there is no marker in the output. Which session we are in is
then read from the environment rather than parsed out of a glyph. The
parser still strips it, because a defence that costs nothing is worth
keeping.

Ten shortened session names currently alive on this machine reproduce
byte for byte, sha256 fingerprint included. That is the test that
mattered: a name is an address, and one character of disagreement with
the Go implementation would leave every shortened session unfindable and
awp starting a second of each. No synthetic corpus can prove that — the
fingerprint has to match a hash some other code computed months ago.

The spawner comes from platform-node-shared, not platfor

packages/server/src/naming.test.ts
@@ -19,10 +19,12 @@ import {
 // is — so what is checked here is the behaviour the rest of the system relies
 // on, which would still have to hold if the implementation were rewritten.
 //
-// What CANNOT be proved here is anything that is a claim about zmx itself:
-// that 46 is really its limit, that `zmx ls` really marks the caller's own
-// session, that it really refuses a slash silently. Those are inherited and
-// unverified — see the probe.
+// What CANNOT be proved here is anything that is a claim about zmx itself.
+// Those went to `bun run probe:claims`, which measured them on 2026-08-25: the
+// 46-character limit holds (47 is refused, and zmx names its own max), and a
+// name containing a slash is refused *silently* — no error, no session, nothing
+// to notice. Only the arrow marking the caller's own session is still untested,
+// because observing it means running inside a session and the probe refuses to.
 
 // A corpus with the shapes that actually occur, including the pair the Go
 // comment names as the reason shortening cannot be plain truncation.
@@ -59,7 +61,9 @@ describe("sanitize", () => {
     expect(sanitize("a.b")).not.toContain(".");
   });
 
-  test("replaces a slash — zmx is claimed to refuse a name containing one", () => {
+  test("replaces a slash — zmx refuses such a name, and says nothing", () => {
+    // Measured: no error, no session, no exit code to notice. A workspace named
+    // after a branch would silently never get one.
     expect(sanitize("feature/thing")).not.toContain("/");
   });
 });
@@ -107,6 +111,96 @@ describe("sessionName", () => {
     for (const { project, workspace, kind } of everyCombination) {
       const split = splitSessionName(sessionName(project, workspace, kind));
       expect(split?.kind).toBe(sessionKind(kind));
+    }
+  });
+});
+
+describe("compatibility with sessions that already exist", () => {
+  // Every one of these was read off `zmx ls` on 2026-08-25 and every one had
+  // been shortened, so each exercises the sha256 fingerprint and the budget
+  // arithmetic together.
+  //
+  // This is the test that matters most in the file. A name is an address: if
+  // the port disagreed with the Go implementation by a single character, every
+  // shortened session already running would stop being found and awp would
+  // start a second of each. A synthetic corpus cannot catch that — only real
+  // names can, because the fingerprint has to match a hash computed by other
+  // code, months ago.
+  //
+  // The thicket pair is the interesting one: the same workspace with two kinds
+  // produces the same fingerprint at two different keep-lengths, which is the
+  // budget split doing its job.
+  const observed: ReadonlyArray<readonly [string, string, string, string]> = [
+    [
+      "typed-router",
+      "effect-ts-v4-endpoint-poc",
+      "agent",
+      "awp.typed-router.effect-ts-v4-endpo-afe7.agent",
+    ],
+    [
+      "orchard",
+      "pr-557-lantern-sentry-header-allowlist",
+      "agent",
+      "awp.orchard.pr-557-lantern-sentry-head-bc47.agent",
+    ],
+    [
+      "orchard",
+      "pr-558-lantern-lantern-identity-hasher",
+      "agent",
+      "awp.orchard.pr-558-lantern-lantern-ide-bfad.agent",
+    ],
+    [
+      "thicket",
+      "effect-ts-tiered-discount-timemachine",
+      "action_dev",
+      "awp.thicket.effect-ts-tiered-d-f500.action_dev",
+    ],
+    [
+      "thicket",
+      "effect-ts-tiered-discount-timemachine",
+      "agent",
+      "awp.thicket.effect-ts-tiered-discou-f500.agent",
+    ],
+    [
+      "thicket",
+      "pr-2320-jordan-survey-slot-cls",
+      "agent",
+      "awp.thicket.pr-2320-jordan-survey-s-a5f9.agent",
+    ],
+    [
+      "thicket",
+      "pr-2340-lantern-sentry-header-allowlist",
+      "agent",
+      "awp.thicket.pr-2340-lantern-sentry-h-6fb6.agent",
+    ],
+    [
+      "thicket",
+      "pr-2357-lantern-lantern-email-link-identity",
+      "agent",
+      "awp.thicket.pr-2357-lantern-lantern-78f6.agent",
+    ],
+    [
+      "thicket",
+      "pr-2359-lantern-lantern-identity-resolve",
+      "agent",
+      "awp.thicket.pr-2359-lantern-lantern-6071.agent",
+    ],
+    [
+      "thicket",
+      "express-2nd-pick-v2-rollback-ship",
+      "agent",
+      "awp.thicket.express-2nd-pick-v2-rol-999a.agent",
+    ],
+  ];
+
+  test.each(observed)("%s / %s / %s → %s", (project, workspace, kind, expected) => {
+    expect(sessionName(project, workspace, kind)).toBe(expected);
+  });
+
+  test("each one really was shortened, or the fingerprint is untested", () => {
+    for (const [project, workspace, kind] of observed) {
+      const stem = splitSessionName(sessionName(project, workspace, kind))?.stem;
+      expect(stem).not.toBe(sessionStem(project, workspace));
     }
   });
 });

packages/server/src/zmx-parse.test.ts
@@ -1,0 +1,186 @@
+import { describe, expect, test } from "vitest";
+import { type Session, identity, isLive } from "./multiplexer.js";
+import { parseSessionLine, parseSessionList, requireName } from "./zmx-parse.js";
+
+// Fixtures are real `zmx ls` output captured on 2026-08-25, not invented. The
+// awkward cases are already in it: a start_dir containing a space, a cmd
+// containing spaces and quotes, a cmd zmx truncated with a literal "...", the
+// arrow marking the caller's own session, and rows both with and without awp's
+// identity labels.
+const t = (...fields: string[]) => fields.join("\t");
+
+const OBSIDIAN = t(
+  "  name=awp.Obsidian_Vault.default.agent",
+  "pid=83768",
+  "clients=0",
+  "created=1787254760",
+  "start_dir=/Users/acohen/Documents/Obsidian Vault",
+  "cmd=claude --permission-mode auto --model opus",
+);
+
+const CALLERS_OWN = t(
+  "→ name=awp.awp.awp-kit-amoeba.agent",
+  "pid=28887",
+  "clients=0",
+  "created=1787662356",
+  "start_dir=/Users/acohen/.awp/workspaces/awp/awp-kit-amoeba",
+  "cmd=claude --permission-mode auto --model opus",
+);
+
+const LABELLED = t(
+  "  name=awp.typed-router.effect-ts-v4-endpo-afe7.agent",
+  "pid=30156",
+  "clients=0",
+  "created=1787575619",
+  "start_dir=/Users/acohen/.awp/workspaces/typed-router/effect-ts-v4-endpoint-poc",
+  "cmd=claude --permission-mode auto --model opus...",
+  "awp_kind=agent",
+  "awp_project=typed-router",
+  "awp_workspace=effect-ts-v4-endpoint-poc",
+);
+
+const QUOTED_CMD = t(
+  "  name=awp.thicket.effect-ts-tiered-d-f500.action_dev",
+  "pid=65040",
+  "clients=0",
+  "created=1787590617",
+  "start_dir=/Users/acohen/.awp/workspaces/thicket/effect-ts-tiered-discount-timemachine",
+  "cmd=sh -c 'pnpm dev'",
+);
+
+describe("parseSessionLine", () => {
+  test("reads the ordinary fields", () => {
+    const session = parseSessionLine(OBSIDIAN);
+    expect(session?.name).toBe("awp.Obsidian_Vault.default.agent");
+    expect(session?.pid).toBe(83768);
+    expect(session?.clients).toBe(0);
+  });
+
+  test("keeps a start_dir containing a space — fields are tab-separated", () => {
+    expect(parseSessionLine(OBSIDIAN)?.startDir).toBe("/Users/acohen/Documents/Obsidian Vault");
+  });
+
+  test("keeps a cmd containing spaces and quotes", () => {
+    expect(parseSessionLine(QUOTED_CMD)?.cmd).toBe("sh -c 'pnpm dev'");
+  });
+
+  test("keeps zmx's own truncation marker rather than pretending it is the command", () => {
+    expect(parseSessionLine(LABELLED)?.cmd).toBe("claude --permission-mode auto --model opus...");
+  });
+
+  test("splits on the first = only, so a value may contain one", () => {
+    const line = t("name=x", "cmd=FOO=bar prog --flag=1");
+    expect(parseSessionLine(line)?.cmd).toBe("FOO=bar prog --flag=1");
+  });
+
+  test("turns created into a Date, not a unix stamp", () => {
+    expect(parseSessionLine(OBSIDIAN)?.created).toEqual(new Date(1787254760 * 1000));
+  });
+
+  test("collects unknown fields as labels", () => {
+    expect(parseSessionLine(LABELLED)?.labels).toEqual({
+      awp_kind: "agent",
+      awp_project: "typed-router",
+      awp_workspace: "effect-ts-v4-endpoint-poc",
+    });
+  });
+
+  test("a session with no labels has none, rather than undefined", () => {
+    expect(parseSessionLine(OBSIDIAN)?.labels).toEqual({});
+  });
+
+  test("is not fooled by a row with no name", () => {
+    expect(parseSessionLine(t("pid=1", "clients=0"))).toBeUndefined();
+  });
+
+  test("reads a non-numeric pid as absent rather than as its own prefix", () => {
+    // "12abc" is 0, not 12. A partially-parsed pid is a pid that addresses the
+    // wrong process.
+    expect(parseSessionLine(t("name=x", "pid=12abc"))?.pid).toBe(0);
+    expect(parseSessionLine(t("name=x", "pid="))?.pid).toBe(0);
+  });
+
+  test("ignores blank lines and text that is not key=value", () => {
+    expect(parseSessionLine("")).toBeUndefined();
+    expect(parseSessionLine("   ")).toBeUndefined();
+    expect(parseSessionLine("no pairs here")).toBeUndefined();
+  });
+});
+
+describe("the caller's own session", () => {
+  // The one that would go missing. Left unstripped, the arrow makes the first
+  // field `→ name=…`, the row fails to parse, and the session that disappears
+  // is always the caller's own — the workspace a developer looks at first.
+  test("is parsed despite the arrow marking it", () => {
+    const session = parseSessionLine(CALLERS_OWN);
+    expect(session?.name).toBe("awp.awp.awp-kit-amoeba.agent");
+    expect(session?.pid).toBe(28887);
+  });
+
+  test("survives a full listing", () => {
+    const all = parseSessionList([OBSIDIAN, CALLERS_OWN, LABELLED].join("\n"));
+    expect(all.map((s) => s.name)).toContain("awp.awp.awp-kit-amoeba.agent");
+    expect(all).toHaveLength(3);
+  });
+});
+
+describe("ended", () => {
+  test("a listed session is not necessarily a running one", () => {
+    // zmx keeps a session listed after its command exits so the output can
+    // still be read. Attaching to one renders a dead program's last screen.
+    const dead = parseSessionLine(t("name=x", "pid=1", "ended=true", "exit_code=2"));
+    expect(dead?.ended).toBe(true);
+    expect(dead?.exitCode).toBe(2);
+    expect(isLive(dead as never)).toBe(false);
+  });
+
+  test("a session without the field is live", () => {
+    expect(isLive(parseSessionLine(OBSIDIAN) as never)).toBe(true);
+  });
+});
+
+describe("identity", () => {
+  test("prefers the labels, which carry the unshortened name", () => {
+    const session = parseSessionLine(LABELLED);
+    expect(identity(session as never)).toEqual({
+      project: "typed-router",
+      workspace: "effect-ts-v4-endpoint-poc",
+      kind: "agent",
+    });
+  });
+
+  test("falls back to the name when a session has no labels yet", () => {
+    // There is a window between a session existing and its labels being set,
+    // during which the name is all there is.
+    expect(identity(parseSessionLine(OBSIDIAN) as never)).toEqual({
+      project: "Obsidian_Vault",
+      workspace: "default",
+      kind: "agent",
+    });
+  });
+
+  test("the fallback is lossy exactly where the labels are not", () => {
+    const session = parseSessionLine(LABELLED);
+    expect(session).toBeDefined();
+    const fromName = identity({ ...(session as Session), labels: {} });
+    // The name was shortened to fit, so reading it back gives the shortened
+    // spelling — which matches no workspace.
+    expect(fromName?.workspace).toBe("effect-ts-v4-endpo-afe7");
+    expect(fromName?.workspace).not.toBe("effect-ts-v4-endpoint-poc");
+  });
+
+  test("reports a session awp did not create as not ours", () => {
+    expect(identity(parseSessionLine(t("name=mysession", "pid=1")) as never)).toBeUndefined();
+  });
+});
+
+describe("requireName", () => {
+  test("refuses an empty name rather than letting zmx decide", () => {
+    expect(requireName("kill", "")).toContain("no name given");
+    expect(requireName("kill", "   ")).toContain("no name given");
+  });
+
+  test("allows a real one", () => {
+    expect(requireName("kill", "awp.a.b.agent")).toBeUndefined();
+  });
+});

andrewcohen/aye

packages/server/src/naming.test.ts
@@ -19,10 +19,12 @@ import {
 // is — so what is checked here is the behaviour the rest of the system relies
 // on, which would still have to hold if the implementation were rewritten.
 //
-// What CANNOT be proved here is anything that is a claim about zmx itself:
-// that 46 is really its limit, that `zmx ls` really marks the caller's own
-// session, that it really refuses a slash silently. Those are inherited and
-// unverified — see the probe.
+// What CANNOT be proved here is anything that is a claim about zmx itself.
+// Those went to `bun run probe:claims`, which measured them on 2026-08-25: the
+// 46-character limit holds (47 is refused, and zmx names its own max), and a
+// name containing a slash is refused *silently* — no error, no session, nothing
+// to notice. Only the arrow marking the caller's own session is still untested,
+// because observing it means running inside a session and the probe refuses to.
 
 // A corpus with the shapes that actually occur, including the pair the Go
 // comment names as the reason shortening cannot be plain truncation.
@@ -59,7 +61,9 @@ describe("sanitize", () => {
     expect(sanitize("a.b")).not.toContain(".");
   });
 
-  test("replaces a slash — zmx is claimed to refuse a name containing one", () => {
+  test("replaces a slash — zmx refuses such a name, and says nothing", () => {
+    // Measured: no error, no session, no exit code to notice. A workspace named
+    // after a branch would silently never get one.
     expect(sanitize("feature/thing")).not.toContain("/");
   });
 });
@@ -107,6 +111,96 @@ describe("sessionName", () => {
     for (const { project, workspace, kind } of everyCombination) {
       const split = splitSessionName(sessionName(project, workspace, kind));
       expect(split?.kind).toBe(sessionKind(kind));
+    }
+  });
+});
+
+describe("compatibility with sessions that already exist", () => {
+  // Every one of these was read off `zmx ls` on 2026-08-25 and every one had
+  // been shortened, so each exercises the sha256 fingerprint and the budget
+  // arithmetic together.
+  //
+  // This is the test that matters most in the file. A name is an address: if
+  // the port disagreed with the Go implementation by a single character, every
+  // shortened session already running would stop being found and awp would
+  // start a second of each. A synthetic corpus cannot catch that — only real
+  // names can, because the fingerprint has to match a hash computed by other
+  // code, months ago.
+  //
+  // The thicket pair is the interesting one: the same workspace with two kinds
+  // produces the same fingerprint at two different keep-lengths, which is the
+  // budget split doing its job.
+  const observed: ReadonlyArray<readonly [string, string, string, string]> = [
+    [
+      "typed-router",
+      "effect-ts-v4-endpoint-poc",
+      "agent",
+      "awp.typed-router.effect-ts-v4-endpo-afe7.agent",
+    ],
+    [
+      "orchard",
+      "pr-557-lantern-sentry-header-allowlist",
+      "agent",
+      "awp.orchard.pr-557-lantern-sentry-head-bc47.agent",
+    ],
+    [
+      "orchard",
+      "pr-558-lantern-lantern-identity-hasher",
+      "agent",
+      "awp.orchard.pr-558-lantern-lantern-ide-bfad.agent",
+    ],
+    [
+      "thicket",
+      "effect-ts-tabular-export-timemachine",
+      "action_dev",
+      "awp.thicket.effect-ts-tiered-d-f500.action_dev",
+    ],
+    [
+      "thicket",
+      "effect-ts-tabular-export-timemachine",
+      "agent",
+      "awp.thicket.effect-ts-tabular-expou-f500.agent",
+    ],
+    [
+      "thicket",
+      "pr-2320-jordan-survey-slot-cls",
+      "agent",
+      "awp.thicket.pr-2320-jordan-survey-s-a5f9.agent",
+    ],
+    [
+      "thicket",
+      "pr-2340-lantern-sentry-header-allowlist",
+      "agent",
+      "awp.thicket.pr-2340-lantern-sentry-h-6fb6.agent",
+    ],
+    [
+      "thicket",
+      "pr-2357-lantern-lantern-email-link-identity",
+      "agent",
+      "awp.thicket.pr-2357-lantern-lantern-78f6.agent",
+    ],
+    [
+      "thicket",
+      "pr-2359-lantern-lantern-identity-resolve",
+      "agent",
+      "awp.thicket.pr-2359-lantern-lantern-6071.agent",
+    ],
+    [
+      "thicket",
+      "express-2nd-pick-v2-rollback-ship",
+      "agent",
+      "awp.thicket.express-2nd-pick-v2-rol-999a.agent",
+    ],
+  ];
+
+  test.each(observed)("%s / %s / %s → %s", (project, workspace, kind, expected) => {
+    expect(sessionName(project, workspace, kind)).toBe(expected);
+  });
+
+  test("each one really was shortened, or the fingerprint is untested", () => {
+    for (const [project, workspace, kind] of observed) {
+      const stem = splitSessionName(sessionName(project, workspace, kind))?.stem;
+      expect(stem).not.toBe(sessionStem(project, workspace));
     }
   });
 });

packages/server/src/zmx-parse.test.ts
@@ -1,0 +1,186 @@
+import { describe, expect, test } from "vitest";
+import { type Session, identity, isLive } from "./multiplexer.js";
+import { parseSessionLine, parseSessionList, requireName } from "./zmx-parse.js";
+
+// Fixtures are real `zmx ls` output captured on 2026-08-25, not invented. The
+// awkward cases are already in it: a start_dir containing a space, a cmd
+// containing spaces and quotes, a cmd zmx truncated with a literal "...", the
+// arrow marking the caller's own session, and rows both with and without awp's
+// identity labels.
+const t = (...fields: string[]) => fields.join("\t");
+
+const OBSIDIAN = t(
+  "  name=awp.Obsidian_Vault.default.agent",
+  "pid=83768",
+  "clients=0",
+  "created=1787254760",
+  "start_dir=/Users/acohen/Documents/Obsidian Vault",
+  "cmd=claude --permission-mode auto --model opus",
+);
+
+const CALLERS_OWN = t(
+  "→ name=awp.awp.awp-kit-amoeba.agent",
+  "pid=28887",
+  "clients=0",
+  "created=1787662356",
+  "start_dir=/Users/acohen/.awp/workspaces/awp/awp-kit-amoeba",
+  "cmd=claude --permission-mode auto --model opus",
+);
+
+const LABELLED = t(
+  "  name=awp.typed-router.effect-ts-v4-endpo-afe7.agent",
+  "pid=30156",
+  "clients=0",
+  "created=1787575619",
+  "start_dir=/Users/acohen/.awp/workspaces/typed-router/effect-ts-v4-endpoint-poc",
+  "cmd=claude --permission-mode auto --model opus...",
+  "awp_kind=agent",
+  "awp_project=typed-router",
+  "awp_workspace=effect-ts-v4-endpoint-poc",
+);
+
+const QUOTED_CMD = t(
+  "  name=awp.thicket.effect-ts-tiered-d-f500.action_dev",
+  "pid=65040",
+  "clients=0",
+  "created=1787590617",
+  "start_dir=/Users/acohen/.awp/workspaces/thicket/effect-ts-tabular-export-timemachine",
+  "cmd=sh -c 'pnpm dev'",
+);
+
+describe("parseSessionLine", () => {
+  test("reads the ordinary fields", () => {
+    const session = parseSessionLine(OBSIDIAN);
+    expect(session?.name).toBe("awp.Obsidian_Vault.default.agent");
+    expect(session?.pid).toBe(83768);
+    expect(session?.clients).toBe(0);
+  });
+
+  test("keeps a start_dir containing a space — fields are tab-separated", () => {
+    expect(parseSessionLine(OBSIDIAN)?.startDir).toBe("/Users/acohen/Documents/Obsidian Vault");
+  });
+
+  test("keeps a cmd containing spaces and quotes", () => {
+    expect(parseSessionLine(QUOTED_CMD)?.cmd).toBe("sh -c 'pnpm dev'");
+  });
+
+  test("keeps zmx's own truncation marker rather than pretending it is the command", () => {
+    expect(parseSessionLine(LABELLED)?.cmd).toBe("claude --permission-mode auto --model opus...");
+  });
+
+  test("splits on the first = only, so a value may contain one", () => {
+    const line = t("name=x", "cmd=FOO=bar prog --flag=1");
+    expect(parseSessionLine(line)?.cmd).toBe("FOO=bar prog --flag=1");
+  });
+
+  test("turns created into a Date, not a unix stamp", () => {
+    expect(parseSessionLine(OBSIDIAN)?.created).toEqual(new Date(1787254760 * 1000));
+  });
+
+  test("collects unknown fields as labels", () => {
+    expect(parseSessionLine(LABELLED)?.labels).toEqual({
+      awp_kind: "agent",
+      awp_project: "typed-router",
+      awp_workspace: "effect-ts-v4-endpoint-poc",
+    });
+  });
+
+  test("a session with no labels has none, rather than undefined", () => {
+    expect(parseSessionLine(OBSIDIAN)?.labels).toEqual({});
+  });
+
+  test("is not fooled by a row with no name", () => {
+    expect(parseSessionLine(t("pid=1", "clients=0"))).toBeUndefined();
+  });
+
+  test("reads a non-numeric pid as absent rather than as its own prefix", () => {
+    // "12abc" is 0, not 12. A partially-parsed pid is a pid that addresses the
+    // wrong process.
+    expect(parseSessionLine(t("name=x", "pid=12abc"))?.pid).toBe(0);
+    expect(parseSessionLine(t("name=x", "pid="))?.pid).toBe(0);
+  });
+
+  test("ignores blank lines and text that is not key=value", () => {
+    expect(parseSessionLine("")).toBeUndefined();
+    expect(parseSessionLine("   ")).toBeUndefined();
+    expect(parseSessionLine("no pairs here")).toBeUndefined();
+  });
+});
+
+describe("the caller's own session", () => {
+  // The one that would go missing. Left unstripped, the arrow makes the first
+  // field `→ name=…`, the row fails to parse, and the session that disappears
+  // is always the caller's own — the workspace a developer looks at first.
+  test("is parsed despite the arrow marking it", () => {
+    const session = parseSessionLine(CALLERS_OWN);
+    expect(session?.name).toBe("awp.awp.awp-kit-amoeba.agent");
+    expect(session?.pid).toBe(28887);
+  });
+
+  test("survives a full listing", () => {
+    const all = parseSessionList([OBSIDIAN, CALLERS_OWN, LABELLED].join("\n"));
+    expect(all.map((s) => s.name)).toContain("awp.awp.awp-kit-amoeba.agent");
+    expect(all).toHaveLength(3);
+  });
+});
+
+describe("ended", () => {
+  test("a listed session is not necessarily a running one", () => {
+    // zmx keeps a session listed after its command exits so the output can
+    // still be read. Attaching to one renders a dead program's last screen.
+    const dead = parseSessionLine(t("name=x", "pid=1", "ended=true", "exit_code=2"));
+    expect(dead?.ended).toBe(true);
+    expect(dead?.exitCode).toBe(2);
+    expect(isLive(dead as never)).toBe(false);
+  });
+
+  test("a session without the field is live", () => {
+    expect(isLive(parseSessionLine(OBSIDIAN) as never)).toBe(true);
+  });
+});
+
+describe("identity", () => {
+  test("prefers the labels, which carry the unshortened name", () => {
+    const session = parseSessionLine(LABELLED);
+    expect(identity(session as never)).toEqual({
+      project: "typed-router",
+      workspace: "effect-ts-v4-endpoint-poc",
+      kind: "agent",
+    });
+  });
+
+  test("falls back to the name when a session has no labels yet", () => {
+    // There is a window between a session existing and its labels being set,
+    // during which the name is all there is.
+    expect(identity(parseSessionLine(OBSIDIAN) as never)).toEqual({
+      project: "Obsidian_Vault",
+      workspace: "default",
+      kind: "agent",
+    });
+  });
+
+  test("the fallback is lossy exactly where the labels are not", () => {
+    const session = parseSessionLine(LABELLED);
+    expect(session).toBeDefined();
+    const fromName = identity({ ...(session as Session), labels: {} });
+    // The name was shortened to fit, so reading it back gives the shortened
+    // spelling — which matches no workspace.
+    expect(fromName?.workspace).toBe("effect-ts-v4-endpo-afe7");
+    expect(fromName?.workspace).not.toBe("effect-ts-v4-endpoint-poc");
+  });
+
+  test("reports a session awp did not create as not ours", () => {
+    expect(identity(parseSessionLine(t("name=mysession", "pid=1")) as never)).toBeUndefined();
+  });
+});
+
+describe("requireName", () => {
+  test("refuses an empty name rather than letting zmx decide", () => {
+    expect(requireName("kill", "")).toContain("no name given");
+    expect(requireName("kill", "   ")).toContain("no name given");
+  });
+
+  test("allows a real one", () => {
+    expect(requireName("kill", "awp.a.b.agent")).toBeUndefined();
+  });
+});