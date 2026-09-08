andrewcohen/awp

feat(jobs): a workspace is still made when the model is unreachable
The naming step runs claude, takes about twelve seconds and needs a network,
and every one of those can be missing - on a train, behind a captive portal,
while the vendor is having a bad afternoon. What a person asked for was a
workspace. Refusing the whole job because the caption could not be composed
loses the work over the label on it, and then rolls back the thread as well.

So an IntentError is caught and the words the person already typed are made
into a directory name:

  "add tiered discounts to checkout"  ->  add-tiered-discounts-to
  "fix the 500 on /api/orders"        ->  fix-the-500-on
  "   "                               ->  workspace

Four words, because a workspace name is a path segment and part of a session
name, and the shortening that makes a long one fit is what makes it
unreadable. The label keeps the whole sentence; a label has room.

This is not a second namer competing with the model. It does the mechanical
half - words into a path segment - and what the model adds is reading the
sentence. nameFrom does not pretend to.

Deterministic, and that matters more than it looks: the step is safe to run
twice, so a retry after a failed later step must produce the same name rather
than a second workspace beside the first.

Said out loud in the panel that is already open, at the moment it happens. A
name that quietly differs from the one a person would have got is a name whose
provenance they have to work out.

The fake in the job's test now fails with a real IntentError rather than the
plain object `act` raises, because the step catches it by tag. A fake failing
with something else would prove the fallback works against an error the real
service cannot produce, and would go on passing if the tag were renamed.

Mutation-checked: removing the catch fails the new test, and nothing else.

packages/server/src/intent-fallback.test.ts
@@ -1,0 +1,52 @@
+import { describe, expect, it } from "vitest";
+import { nameFrom } from "./intent";
+
+// The name made here when the model cannot be reached.
+//
+// Kept out of intent.test.ts because that file is about the model call — the
+// flags, the fishing of JSON out of prose, the timeout. This is a pure function
+// over a sentence, and the two have nothing to say to each other.
+
+describe("naming a workspace without the model", () => {
+  it("takes the first four words", () => {
+    expect(nameFrom("add tiered discounts to checkout").name).toBe("add-tiered-discounts-to");
+  });
+
+  it("keeps digits and drops punctuation", () => {
+    // A sentence naming a path or a status code is the ordinary case, and both
+    // arrive full of characters a directory name cannot hold.
+    expect(nameFrom("fix the 500 on /api/orders").name).toBe("fix-the-500-on");
+  });
+
+  it("answers something for a sentence with no words in it", () => {
+    // `workspace` rather than a random string: the second time it happens, the
+    // collision is the useful signal.
+    expect(nameFrom("   ").name).toBe("workspace");
+    expect(nameFrom("!!! ??? ...").name).toBe("workspace");
+  });
+
+  it("is deterministic, because the step is safe to run twice", () => {
+    // A retry after a failed later step must produce the same name, or the
+    // second attempt builds a second workspace beside the first.
+    const said = "rewrite the importer";
+    expect(nameFrom(said)).toEqual(nameFrom(said));
+  });
+
+  it("keeps the whole sentence as the label", () => {
+    // The name is a path segment and part of a session name, so it is cut
+    // short. A label has room, and losing the sentence there would be losing
+    // it for nothing.
+    const said = "add tiered discounts to checkout for the enterprise plan";
+    expect(nameFrom(said).label).toBe(said);
+    expect(nameFrom(said).prompt).toBe(said);
+  });
+
+  it("trims the label but not into nothing", () => {
+    expect(nameFrom("  tidy up  ").label).toBe("tidy up");
+    expect(nameFrom("   ").label).toBe("workspace");
+  });
+
+  it("lowercases, because a name is an address and case is not part of it", () => {
+    expect(nameFrom("Fix The Importer").name).toBe("fix-the-importer");
+  });
+});

packages/server/src/jobs/create-workspace.test.ts
@@ -5,7 +5,7 @@ import { Context, Effect, Layer } from "effect";
 import { beforeEach, describe, expect, test } from "vitest";
 import type { Jj } from "../jj";
 import type { Multiplexer } from "../multiplexer";
-import type { WorkspaceIntent } from "../intent";
+import { IntentError, type WorkspaceIntent } from "../intent";
 import type { Settings } from "../settings";
 import type { Threads } from "../threads";
 import {
@@ -96,8 +96,14 @@ const deps = (): WorkspaceDeps => ({
   // already, so it short-circuits and neither is touched — which is both the
   // resume path and what keeps these tests about the order of the steps.
   intent: {
+    // Fails with a real `IntentError` rather than the plain object `act`
+    // raises, because the step catches it *by tag*. A fake that failed with
+    // something else would prove the fallback works against an error the real
+    // service cannot produce — and would go on passing if the tag were
+    // renamed.
     resolve: (description: string) =>
       act(`intent(${description})`).pipe(
+        Effect.mapError(() => new IntentError({ reason: "claude is not there" })),
         Effect.as({ name: "named-by-the-model", label: "Named by the model", prompt: undefined }),
       ),
   } as unknown as WorkspaceIntent["Service"],
@@ -199,6 +205,39 @@ describe("naming the workspace", () => {
       label: "Named by the model",
       bookmark: "andrew/named-by-the-model",
     });
+  });
+
+  // ── the model being unreachable is not a reason to lose the work ──────
+  //
+  // A subprocess, twelve seconds and a network, any of which can be missing.
+  // What was asked for is a workspace, and refusing the whole job because the
+  // caption could not be composed loses the work over the label on it.
+  test("a workspace is still made when the model cannot be reached", async () => {
+    breaking.add("intent(add tiered discounts to checkout)");
+
+    const job = await make({
+      workspace: undefined,
+      label: undefined,
+      prompt: undefined,
+      bookmark: undefined,
+    });
+
+    expect(job.status).toBe("succeeded");
+    // Named from the words the person typed — four of them, made into a path
+    // segment. Not a second namer: what the model adds is reading the
+    // sentence, and this does not pretend to.
+    expect(job.input).toMatchObject({
+      workspace: "add-tiered-discounts-to",
+      label: "add tiered discounts to checkout",
+      bookmark: "andrew/add-tiered-discounts-to",
+    });
+    // And the later steps built on it, which is the part that would break if
+    // the patch were merged anywhere other than the stored input.
+    expect(trace).toContain("jj.add(add-tiered-discounts-to)");
+    expect(trace).toContain("thread.claim(20260826-aaaa:add-tiered-discounts-to)");
+    // Nothing was rolled back. A job that failed here would have unwound the
+    // thread it was building for.
+    expect(trace.filter((entry) => entry.startsWith("thread.delete("))).toEqual([]);
   });
 
   // Ten seconds and a second, different answer is what the alternative costs,

andrewcohen/aye

  "add tabular exports to checkout"  ->  add-tabular-exports-to

packages/server/src/intent-fallback.test.ts
@@ -1,0 +1,52 @@
+import { describe, expect, it } from "vitest";
+import { nameFrom } from "./intent";
+
+// The name made here when the model cannot be reached.
+//
+// Kept out of intent.test.ts because that file is about the model call — the
+// flags, the fishing of JSON out of prose, the timeout. This is a pure function
+// over a sentence, and the two have nothing to say to each other.
+
+describe("naming a workspace without the model", () => {
+  it("takes the first four words", () => {
+    expect(nameFrom("add tabular exports to checkout").name).toBe("add-tabular-exports-to");
+  });
+
+  it("keeps digits and drops punctuation", () => {
+    // A sentence naming a path or a status code is the ordinary case, and both
+    // arrive full of characters a directory name cannot hold.
+    expect(nameFrom("fix the 500 on /api/orders").name).toBe("fix-the-500-on");
+  });
+
+  it("answers something for a sentence with no words in it", () => {
+    // `workspace` rather than a random string: the second time it happens, the
+    // collision is the useful signal.
+    expect(nameFrom("   ").name).toBe("workspace");
+    expect(nameFrom("!!! ??? ...").name).toBe("workspace");
+  });
+
+  it("is deterministic, because the step is safe to run twice", () => {
+    // A retry after a failed later step must produce the same name, or the
+    // second attempt builds a second workspace beside the first.
+    const said = "rewrite the importer";
+    expect(nameFrom(said)).toEqual(nameFrom(said));
+  });
+
+  it("keeps the whole sentence as the label", () => {
+    // The name is a path segment and part of a session name, so it is cut
+    // short. A label has room, and losing the sentence there would be losing
+    // it for nothing.
+    const said = "add tabular exports to checkout for the enterprise plan";
+    expect(nameFrom(said).label).toBe(said);
+    expect(nameFrom(said).prompt).toBe(said);
+  });
+
+  it("trims the label but not into nothing", () => {
+    expect(nameFrom("  tidy up  ").label).toBe("tidy up");
+    expect(nameFrom("   ").label).toBe("workspace");
+  });
+
+  it("lowercases, because a name is an address and case is not part of it", () => {
+    expect(nameFrom("Fix The Importer").name).toBe("fix-the-importer");
+  });
+});

packages/server/src/jobs/create-workspace.test.ts
@@ -5,7 +5,7 @@ import { Context, Effect, Layer } from "effect";
 import { beforeEach, describe, expect, test } from "vitest";
 import type { Jj } from "../jj";
 import type { Multiplexer } from "../multiplexer";
-import type { WorkspaceIntent } from "../intent";
+import { IntentError, type WorkspaceIntent } from "../intent";
 import type { Settings } from "../settings";
 import type { Threads } from "../threads";
 import {
@@ -96,8 +96,14 @@ const deps = (): WorkspaceDeps => ({
   // already, so it short-circuits and neither is touched — which is both the
   // resume path and what keeps these tests about the order of the steps.
   intent: {
+    // Fails with a real `IntentError` rather than the plain object `act`
+    // raises, because the step catches it *by tag*. A fake that failed with
+    // something else would prove the fallback works against an error the real
+    // service cannot produce — and would go on passing if the tag were
+    // renamed.
     resolve: (description: string) =>
       act(`intent(${description})`).pipe(
+        Effect.mapError(() => new IntentError({ reason: "claude is not there" })),
         Effect.as({ name: "named-by-the-model", label: "Named by the model", prompt: undefined }),
       ),
   } as unknown as WorkspaceIntent["Service"],
@@ -199,6 +205,39 @@ describe("naming the workspace", () => {
       label: "Named by the model",
       bookmark: "andrew/named-by-the-model",
     });
+  });
+
+  // ── the model being unreachable is not a reason to lose the work ──────
+  //
+  // A subprocess, twelve seconds and a network, any of which can be missing.
+  // What was asked for is a workspace, and refusing the whole job because the
+  // caption could not be composed loses the work over the label on it.
+  test("a workspace is still made when the model cannot be reached", async () => {
+    breaking.add("intent(add tabular exports to checkout)");
+
+    const job = await make({
+      workspace: undefined,
+      label: undefined,
+      prompt: undefined,
+      bookmark: undefined,
+    });
+
+    expect(job.status).toBe("succeeded");
+    // Named from the words the person typed — four of them, made into a path
+    // segment. Not a second namer: what the model adds is reading the
+    // sentence, and this does not pretend to.
+    expect(job.input).toMatchObject({
+      workspace: "add-tabular-exports-to",
+      label: "add tabular exports to checkout",
+      bookmark: "andrew/add-tabular-exports-to",
+    });
+    // And the later steps built on it, which is the part that would break if
+    // the patch were merged anywhere other than the stored input.
+    expect(trace).toContain("jj.add(add-tabular-exports-to)");
+    expect(trace).toContain("thread.claim(20260826-aaaa:add-tabular-exports-to)");
+    // Nothing was rolled back. A job that failed here would have unwound the
+    // thread it was building for.
+    expect(trace.filter((entry) => entry.startsWith("thread.delete("))).toEqual([]);
   });
 
   // Ten seconds and a second, different answer is what the alternative costs,